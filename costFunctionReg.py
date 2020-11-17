e236e2516a5fdede32b32e8929c1d0556ba6d98a
kimalamsyah <44354552+kimalamsyah@users.noreply.github.com>
2020-11-17 15:02:13 +0700

    Update and rename costFunction.py to costFunctionReg.py

--- costFunctionReg.py
+++ costFunctionReg.py
@@ -1,4 +1,5 @@
-def costFunctionReg(theta, X, y):
+#costFunction with regularization
+def costFunctionReg(theta, X, y, l):
     #initializing terms
     m = X.shape[1]
     h = sigmoid(X.dot(theta))
@@ -8,5 +9,12 @@ def costFunctionReg(theta, X, y):
     #cost function
     J = -(1 / m) * (y.transpose().dot(log(h)) + (np.subtract(1, y)).transpose().dot(log(1-h))) + (1 / (2*m)) * np.matmul(thetaR.T, thetaR)
     
-    return J
     
+    #gradient descent
+    delta = np.subtract(h, y)
+    sumdelta = np.sum(delta)
+    
+    grad = (1 / m) * (sumdelta * X.transpose()) + (l / m) * thetaR
+    grad[0] = 0
+
+    return J, grad.flatten()
