6486423bb7d0a45c1a4ca219627a3a669c93d407
kimalamsyah <44354552+kimalamsyah@users.noreply.github.com>
2020-11-17 15:27:44 +0700

    Update costFunctionReg.py

--- costFunctionReg.py
+++ costFunctionReg.py
@@ -5,8 +5,8 @@ def costFunctionReg(theta, X, y, l):
     h = sigmoid(X.dot(theta))
     
     #cost function
-    J = -(1 / m) * (y.transpose().dot(log(h)) + (np.subtract(1, y)).transpose().dot(log(1-h))) + ( l / (2*m)) * np.sum(np.power(theta[1:, 0]))
-    
+    J = -(1 / m) * (y.transpose().dot(log(h)) + (np.subtract(1, y)).transpose().dot(log(1-h))) + ( l / (2*m)) * np.sum(np.power((theta[1:, 0]), 2))
+
     #gradient descent
     delta = np.subtract(h, y)
     
